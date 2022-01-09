410a85456dbad2aaa97b931610b38db603fccc3f
rilp98 <paolaanttonelar23@gmail.com>
2022-01-09 10:36:51 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+
+const REVIEWS = [
+  { id:1, name:'Susan', rol:"web developer", description:"Text1"  },
+  { id:2, name:'Olvia', rol:"UI/UX", description:"Text2"  },
+  { id:3, name:'Robert', rol:"Project manager", description:"Text3"  },
+]
 
 function App() {
+
+  const [data,setData] = useState(REVIEWS[0]);
+  const [num,setNum] =useState(3);
+  
+  const handleData= ()=>{ 
+    const resultado = REVIEWS.find( item => item.id === num );
+    setData(resultado);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{data.name}</p>
+      <p>{data.id}</p>
+     <button onClick={handleData}>Siguiente</button>
     </div>
   );
 }
